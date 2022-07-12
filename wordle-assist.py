da9deb4b2a831f9ccb9f572ce71ac9e7ed7e0b1f
David Sturgis <david-sturgis@pluralsight.com>
2022-07-12 01:24:07 +0000

    Collaborative solution from 2022-07-11

--- wordle-assist.py
+++ wordle-assist.py
@@ -10,14 +10,40 @@ def load_five_letter_words():
             possible_words.append(word)
     return possible_words
 
+green = {'r': 2}
+gray = ['salnchumptid']
+yellow = {'o': 3, 'e': 3}
 
 def wordle_assist(possible_words):
     starting_word_count = len(possible_words)
     print(f"Starting with {starting_word_count:,} possible 5-letter words:")
 
-    print(", ".join(possible_words))
+    new_possible_words = list()
+    for word in possible_words:
+        keep_word = True
+        # Green letter hints
+        for letter, position in green.items():
+            if word[position] != letter:
+                keep_word = False
 
-    ending_word_count = len(possible_words)
+        # Yellow letter hints
+        for letter, position in yellow.items():
+            if letter not in word:
+                keep_word = False
+            if word[position] == letter:
+                keep_word = False
+        
+        # Gray letter hints
+        for ch in gray[0]:
+            if ch in word:
+                keep_word = False
+                
+        if keep_word:
+            new_possible_words.append(word)
+
+    print(", ".join(new_possible_words))
+
+    ending_word_count = len(new_possible_words)
     decrease = starting_word_count - ending_word_count
     decrease_percent = round((decrease / starting_word_count) * 100, 2) if decrease else decrease
     print(f"Ending with {ending_word_count:,} remaining 5-letter words, a decrease of {decrease_percent}%")
